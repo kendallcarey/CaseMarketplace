63eba632e864847818dd8ed551b48180fb96cd82
Kendall Carey <kcarey@dayspring-tech.com>
2016-04-07 20:45:28 -0700

    user auth basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+
   resources :cases
 
   # The priority is based upon order of creation: first created -> highest priority.
